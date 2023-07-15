9c2c84ce0653c644fe97506f72c54bb45d0a418c
sjinzh <99076655+sjinzh@users.noreply.github.com>
2023-07-15 23:00:56 +0800

    update

--- src/lib.zig
+++ src/lib.zig
@@ -1,12 +1,12 @@
 pub const rpc = struct {
-    usingnamespace @import("./rpc/client.zig");
+    pub usingnamespace @import("./rpc/client.zig");
     const types = struct {
-        usingnamespace @import("./rpc/types.zig");
+        pub usingnamespace @import("./rpc/types.zig");
     };
 };
 
 pub const core = struct {
-    usingnamespace @import("./core/pubkey.zig");
-    usingnamespace @import("./core/account.zig");
-    usingnamespace @import("./core/transaction.zig");
+    pub usingnamespace @import("./core/pubkey.zig");
+    pub usingnamespace @import("./core/account.zig");
+    pub usingnamespace @import("./core/transaction.zig");
 };
